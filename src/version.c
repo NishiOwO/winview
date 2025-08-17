5e925e6e955cf05ca84c9f639f55cd51896ff098
NishiOwO <nishi@nishi.boats>
2025-08-17 16:29:55 +0900

    well

--- src/version.c
+++ src/version.c
@@ -1,23 +1,8 @@
 #include <wvcommon.h>
 #include <wvresource.h>
 
-#define USE_STAR
-
 const char* wvversion = "0.0";
 
-#ifdef USE_STAR
-#define STARS 200
-#define FPS 30
-
-typedef struct star {
-	double x;
-	double y;
-	double speed;
-	HBRUSH brush;
-} star_t;
-
-static star_t stars[STARS];
-#endif
 static const char* texts[] = {
 	"https://github.com/nishiowo/winview",
 	"",
@@ -32,26 +17,6 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
 	if(msg == WM_CLOSE){
 		EndDialog(hWnd, 0);
 	}else if(msg == WM_INITDIALOG){
-#ifdef USE_STAR
-		RECT r;
-		int ww, wh;
-		int i;
-
-		GetClientRect(hWnd, &r);
-		wh = r.bottom - r.top;
-		ww = r.right - r.left;
-
-		for(i = 0; i < STARS; i++){
-			int c = rand() % 256;
-			stars[i].x = rand() % ww;
-			stars[i].y = rand() % wh;
-
-			stars[i].speed = (double)c / 255;
-			stars[i].brush = GetSolidBrushCached(c, c, c);
-		}
-
-		SetTimer(hWnd, 100, 1000/FPS, NULL);
-#endif
 	}else if(msg == WM_PAINT){
 		PAINTSTRUCT ps;
 		RECT r;
@@ -59,6 +24,7 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
 		int ww, wh;
 		int x, y;
 		int i;
+		int vw;
 
 		GetClientRect(hWnd, &r);
 		wh = r.bottom - r.top;
@@ -67,24 +33,6 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
 		SetTextAlign(dc, TA_CENTER | TA_BOTTOM);
 		SetBkMode(dc, TRANSPARENT);
 
-#ifdef USE_STAR
-		r.left = 0;
-		r.right = ww;
-		r.top = 0;
-		r.bottom = wh;
-		FillRect(dc, &r, GetSolidBrushCached(0, 0, 0));
-		for(i = 0; i < STARS; i++){
-			r.left = stars[i].x;
-			r.top = stars[i].y;
-			r.right = r.left + 2;
-			r.bottom = r.top + 2;
-			FillRect(dc, &r, stars[i].brush);
-
-			stars[i].x -= (double)ww / FPS / 4 * stars[i].speed;
-			if(stars[i].x <= 0) stars[i].x = ww + stars[i].x;
-		}
-#else
-
 		/* 200x138 */
 		for(y = 0; y < (wh / 138) + 1; y++){
 			for(x = 0; x < (ww / 200) + 1; x++){
@@ -92,8 +40,10 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
 			}
 		}
 
-		y = 0;
-		for(i = 0; texts[i] != NULL; i++)y += texts[i][0] == '+' ? 14*5 : 14;
+		y = 10;
+		for(i = 0; texts[i] != NULL; i++){
+			y += texts[i][0] == '+' ? 14*5 : 14;
+		}
 
 		r.left = 0;
 		r.right = ww;
@@ -101,9 +51,8 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
 		r.bottom = wh;
 
 		FillRect(dc, &r, GetSolidBrushCached(0, 0, 0));
-#endif
 
-		y = 0;
+		y = 5;
 		for(i = 0; texts[i] != NULL; i++){
 			int l = strlen(texts[i]) - ((texts[i][0] == '+' || texts[i][0] == '-') ? 1 : 0);
 			char t[512];
@@ -126,12 +75,6 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
 		}
 
 		EndPaint(hWnd, &ps);
-	}else if(msg == WM_TIMER){
-#ifdef USE_STAR
-		if(wp == 100){
-			InvalidateRect(hWnd, NULL, FALSE);
-		}
-#endif
 	}else{
 		return FALSE;
 	}
