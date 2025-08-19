1e29435542e2e8526be7013eed163163d98cc7c0
NishiOwO <nishi@nishi.boats>
2025-08-19 11:55:35 +0900

    fix

--- mk/watcom.mk
+++ mk/watcom.mk
@@ -1,5 +1,6 @@
 AR = ./tools/wlib-ar.pl
 CC = ./tools/owcc.pl
+RC = true
 CFLAGS += -b win32 -I$(WATCOM)/h/nt -std=c99
 LDFLAGS += -b win32
 AFTERCMD = wrc -q -i=include -i=$(WATCOM)/h/nt -bt=nt src/winview.rc winview.exe
