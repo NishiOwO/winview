9d1f12034373b5cdc2721a40b1de0d6a48ab757b
NishiOwO <nishi@nishi.boats>
2025-08-21 16:34:45 +0900

    nicer version

--- src/ui/version.c
+++ src/ui/version.c
@@ -1,6 +1,21 @@
 #include <wvcommon.h>
 #include <wvresource.h>
 
+#define USE_STAR
+
+#ifdef USE_STAR
+#define FPS 30
+
+typedef struct star {
+	double x;
+	double y;
+	double c;
+	HBRUSH brush;
+} star_t;
+
+static star_t stars[256];
+#endif
+
 const char* wvversion = "0.0";
 
 static const char* texts[] = {
@@ -13,19 +28,56 @@ static const char* texts[] = {
     NULL							   /**/
 };
 
+static HBITMAP bufferbmp;
+static HDC bufferdc;
+
 LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 	if(msg == WM_CLOSE) {
 		EndDialog(hWnd, 0);
 	} else if(msg == WM_KEYDOWN) {
 		if(wp == 'Q' || wp == VK_ESCAPE) EndDialog(hWnd, 0);
+	} else if(msg == WM_DESTROY){
+		DeleteDC(bufferdc);
+		DeleteObject(bufferbmp);
 	} else if(msg == WM_INITDIALOG) {
 		RECT r;
+		HDC dc;
+		int i;
 		GetWindowRect(hWnd, &r);
 		SetWindowPos(hWnd, NULL, 0, 0, 440, 440 / 4 * 3, SWP_NOMOVE);
+
+#ifdef USE_STAR
+		dc = GetDC(hWnd);
+
+		GetClientRect(hWnd, &r);
+
+		bufferbmp = CreateCompatibleBitmap(dc, r.right - r.left, r.bottom - r.top);
+		bufferdc = CreateCompatibleDC(dc);
+
+		SelectObject(bufferdc, bufferbmp);
+
+		ReleaseDC(hWnd, dc);
+
+		for(i = 0; i < sizeof(stars) / sizeof(stars[0]); i++){
+			stars[i].x = rand() % (r.right - r.left);
+			stars[i].y = rand() % (r.bottom - r.top);
+			stars[i].c = rand() % 256;
+			stars[i].brush = GetSolidBrushCached(stars[i].c, stars[i].c, stars[i].c);
+
+			stars[i].c /= 255;
+		}
+
+		SetTimer(hWnd, 100, 1000/FPS, NULL);
+#endif
+	} else if(msg == WM_ERASEBKGND){
+#ifdef USE_STAR
+	} else if(msg == WM_TIMER){
+		InvalidateRect(hWnd, NULL, FALSE);
+#endif
 	} else if(msg == WM_PAINT) {
 		PAINTSTRUCT ps;
 		RECT	    r;
-		HDC	    dc = BeginPaint(hWnd, &ps);
+		HDC	    dc = bufferdc;
 		int	    ww, wh;
 		int	    x, y;
 		int	    i;
@@ -38,6 +90,21 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 		SetTextAlign(dc, TA_CENTER | TA_BOTTOM);
 		SetBkMode(dc, TRANSPARENT);
 
+		PatBlt(dc, 0, 0, ww, wh, BLACKNESS);
+
+#ifdef USE_STAR
+		for(i = 0; i < sizeof(stars) / sizeof(stars[0]); i++){
+			r.left = stars[i].x;
+			r.top = stars[i].y;
+			r.right = r.left + 2;
+			r.bottom = r.top + 2;
+
+			FillRect(dc, &r, stars[i].brush);
+
+			stars[i].x -= (double)ww / FPS * stars[i].c / 4;
+			if(stars[i].x <= -2) stars[i].x = ww + stars[i].x + 2;
+		}
+#else
 		/* 200x138 */
 		for(y = 0; y < (wh / 138) + 1; y++) {
 			for(x = 0; x < (ww / 200) + 1; x++) {
@@ -56,6 +123,7 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 		r.bottom = wh;
 
 		FillRect(dc, &r, GetSolidBrushCached(0, 0, 0));
+#endif
 
 		y = 5;
 		for(i = 0; texts[i] != NULL; i++) {
@@ -79,6 +147,9 @@ LRESULT CALLBACK VersionDialog(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 			y += texts[i][0] == '+' ? 14 * 5 : 14;
 		}
 
+		dc = BeginPaint(hWnd, &ps);
+		SetStretchBltMode(dc, HALFTONE);
+		StretchBlt(dc, 0, 0, ww, wh, bufferdc, 0, 0, ww, wh, SRCCOPY);
 		EndPaint(hWnd, &ps);
 	} else {
 		return FALSE;
