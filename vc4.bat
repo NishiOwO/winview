748c28176f95cc0dc629b9be7bd9e24dd696250a
NishiOwO <nishi@nishi.boats>
2025-08-25 20:30:54 +0900

    vc4 build file

---
+++ vc4.bat
@@ -0,0 +1,17 @@
+@echo off
+rd /s build
+md build
+cd build
+
+md wv
+cd wv
+cl /c /D__inline__= /DPBS_SMOOTH=0 /DGetWindowLongPtr=GetWindowLong /DULONG_PTR=ULONG /DDOXPM /DDOXBM /DDOPNG /DDOJPEG /DDOTGA /DDOMSP /I..\..\external\libz\include /I..\..\external\libpng\include /I..\..\external\libjpeg\include /I..\..\include ..\..\src\*.c ..\..\src\ui\*.c ..\..\src\format\*.c
+rc /fowinview.res /i..\..\include ..\..\src\winview.rc
+cd ..
+
+md extern
+cd extern
+cl /c /D__inline__= /DPBS_SMOOTH=0 /DGetWindowLongPtr=GetWindowLong /DULONG_PTR=ULONG /DDOXPM /DDOXBM /DDOPNG /DDOJPEG /DDOTGA /DDOMSP /I..\..\external\libz\include /I..\..\external\libpng\include /I..\..\external\libjpeg\include /I..\..\include ..\..\external\libz\src\*.c ..\..\external\libpng\src\*.c ..\..\external\libjpeg\src\*.c
+cd ..
+
+link /out:winview.exe wv\*.obj extern\*.obj wv\*.res gdi32.lib comctl32.lib comdlg32.lib
